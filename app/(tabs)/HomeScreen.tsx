58e9ef70a336a45cabf1ec8b9ecd970e618710bf
icaro <icarorenantrabalho@hotmail.com>
2026-05-04 16:31:27 -0300

    update homeScreen

--- app/(tabs)/HomeScreen.tsx
+++ app/(tabs)/HomeScreen.tsx
@@ -1,12 +1,30 @@
-import { StyleSheet, Text } from "react-native";
+import { useUserStore } from "@/stores/userStore";
+import { StyleSheet, Text, View } from "react-native";
 import ScreenBackground from "../components/ScreenBackground";
 
 export default function HomeScreen() {
+  const { user } = useUserStore();
   return (
     <ScreenBackground>
-      <Text> Bom dia</Text>
+      <View style={styles.header}>
+        <View>
+          <Text>Portal de Vendas</Text>
+
+          <Text>Olá, {user?.username}</Text>
+        </View>
+        <View style={styles.iconRow}></View>
+      </View>
     </ScreenBackground>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  iconRow: {
+    flexDirection: "row",
+    gap: 4,
+  },
+});
