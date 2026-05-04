42b7486b63e11efe4aa6ceda47d9e92c32d5ecc9
icaro <icarorenantrabalho@hotmail.com>
2026-05-04 16:03:51 -0300

    Adicionando função para voltar nas telas

--- app/components/ScreenBackground.tsx
+++ app/components/ScreenBackground.tsx
@@ -1,17 +1,22 @@
+import { Colors } from "@/constants/colors";
+import Feather from "@expo/vector-icons/Feather";
+import { router } from "expo-router";
 import React, { ReactNode } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 type MuiltColorBackgroundProps = {
   children: ReactNode;
   headerColor?: string;
   headerHeight?: number;
+  useBackAction?: boolean;
 };
 
 export default function ScreenBackground({
   children,
   headerColor = "#16a34a",
   headerHeight = 150,
+  useBackAction = false,
 }: MuiltColorBackgroundProps) {
   const insets = useSafeAreaInsets();
 
@@ -25,7 +30,15 @@ export default function ScreenBackground({
             height: headerHeight + insets.top,
           },
         ]}
-      />
+      >
+        {" "}
+        {useBackAction && (
+          <TouchableOpacity style={styles.touch} onPress={() => router.back()}>
+            <Feather name="arrow-left" size={24} color={Colors.white} />{" "}
+            <Text style={styles.text}>Voltar</Text>
+          </TouchableOpacity>
+        )}
+      </View>
 
       <View
         style={[
@@ -53,8 +66,17 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
   },
+  touch: {
+    flexDirection: "row",
+    gap: 8,
+    padding: 16,
+    alignItems: "center",
+  },
   content: {
     flex: 1,
     alignItems: "center",
+  },
+  text: {
+    color: Colors.white,
   },
 });
